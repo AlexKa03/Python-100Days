9d98fa9170379a8fa4a8f7010fcf9e541e7b6acf
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2025-10-14 18:52:43 +0300

    Day 16
    Object Oriented Programming (OOP)

---
+++ Day016/1.constructing_objects.py
@@ -0,0 +1,13 @@
+# import another_modul
+# print(another_modul.another_variable)
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.fd(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
