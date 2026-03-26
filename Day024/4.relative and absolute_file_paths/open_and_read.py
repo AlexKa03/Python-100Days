4f67e565b3f4357a1e05632ca0ace6738ab11fd5
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2026-03-26 03:19:52 +0200

    Removing old pushes

--- Day024/4.relative and absolute_file_paths/open_and_read.py
+++
@@ -1,9 +0,0 @@
-# Absolute file path example:
-# with open("/Users/USERNAME/Desktop/my_file.txt") as file:
-#     content = file.read()
-#     print(content)
-
-# Relative file path example:
-with open("../../Desktop/my_file.txt") as file:
-    content = file.read()
-    print(content)
