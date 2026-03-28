0436986b5c7b6ebd358cbf239708bc6000e64c51
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2026-03-28 23:55:18 +0200

    Day 17

---
+++ Day017/2.0.attributes_class_constructors.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+user_1 = User("001", "Alex")
+user_2 = User("002", "Bobi")
